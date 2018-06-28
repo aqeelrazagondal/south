44ea92a644eb4e6840bac39655b4ea507dbaea39
aqeelraz146@gmail.com <aqeelrazagondal@gmail.com>
2018-06-28 18:07:38 +0500

    chnage in app.js

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
-var connect = require('connect');
+var express = require('express');
+var path = require('path');
 var serveStatic = require('serve-static');
 
-connect().use(serveStatic(__dirname)).listen(8080, function(){
-    console.log('Server running on 8080...');
+var app = express();
+
+app.use(serveStatic(__dirname));
+app.use(serveStatic(path.join(__dirname, 'public')));
+app.use('/', function (res, res){
+    res.render('index.html');
 });
+
+app.listen(3000, function() {
+    console.log('Server is running on Port 3000');
+});
+
+// var connect = require('connect');
+// var serveStatic = require('serve-static');
+//
+// var express = require('express');
+// var app = express();
+//
+// connect().use(serveStatic(__dirname)).listen(8080, function(){
+//     console.log('Server running on 8080...');
+// });
