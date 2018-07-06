21a57aa281d46309350862d18adda5fb92fee7bd
aqeelraz146@gmail.com <aqeelrazagondal@gmail.com>
2018-07-07 00:31:51 +0500

    update app.js

--- app.js
+++ app.js
@@ -7,11 +7,32 @@ var app = express();
 app.use(serveStatic(__dirname));
 app.use(serveStatic(path.join(__dirname, 'public')));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
 
 app.listen(3000, function() {
     console.log('App IS RUNNING ON PORT 3000');
 });
 
+
+// const express = require('express');
+// const http = require('http')
+// const path = require('path');
+
+// const app = express();
+
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname + '/index.html'));
+// });
+
+// const port = process.env.PORT || 3000;
+// app.set('port', port);
+
+// const server = http.createServer(app);
+// server.listen(port, () => console.log('Server Running at Port '+ port));
 // var connect = require('connect');
 // var serveStatic = require('serve-static');
 //
